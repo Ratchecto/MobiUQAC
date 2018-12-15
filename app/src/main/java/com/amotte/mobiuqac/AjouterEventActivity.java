138bc3fe5a42b9030395f4fc57f2e4f346a357bb
mkava <thomas.rebillard@etu.univ-tours.fr>
2018-12-15 22:40:49 +0100

    Intent Photos et gallerie

--- app/src/main/java/com/amotte/mobiuqac/AjouterEventActivity.java
+++ app/src/main/java/com/amotte/mobiuqac/AjouterEventActivity.java
@@ -1,13 +1,89 @@
 package com.amotte.mobiuqac;
 
+
+import android.app.Activity;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AjouterEventActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 100;
+    File destination;
+
+    private int SELECT_IMAGE = 1;
+
+    private TextView test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__event_);
+
+        test = (TextView) findViewById(R.id.title);
+
     }
+
+    public void onClick(View view) {
+        Intent intent = new Intent();
+// Show only images, no videos or anything else
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+// Always show the chooser (if there are multiple options available)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
+    }
+
+    public void takePhoto(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+            super.onActivityResult(requestCode, resultCode, data);
+
+            if (requestCode == SELECT_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+                Uri uri = data.getData();
+
+                try {
+                    Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                    // Log.d(TAG, String.valueOf(bitmap));
+
+                    ImageView imageView = (ImageView) findViewById(R.id.imagatest);
+                    imageView.setImageBitmap(bitmap);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            // Log.d(TAG, String.valueOf(bitmap));
+
+            ImageView imageView = (ImageView) findViewById(R.id.imagatest);
+            imageView.setImageBitmap(imageBitmap);
+        }
+
+    }
+
+
 }
