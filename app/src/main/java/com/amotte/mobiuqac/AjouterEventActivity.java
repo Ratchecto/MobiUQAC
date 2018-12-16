864ae95047c95027dc4b5b7b43d2f006c055ebeb
mkava <thomas.rebillard@etu.univ-tours.fr>
2018-12-16 02:47:46 +0100

    Boouton enregistrer evenement

--- app/src/main/java/com/amotte/mobiuqac/AjouterEventActivity.java
+++ app/src/main/java/com/amotte/mobiuqac/AjouterEventActivity.java
@@ -2,6 +2,7 @@ package com.amotte.mobiuqac;
 
 
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
@@ -15,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,6 +24,7 @@ import android.widget.Toast;
 import java.io.File;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class AjouterEventActivity extends AppCompatActivity {
@@ -31,55 +34,65 @@ public class AjouterEventActivity extends AppCompatActivity {
 
     ImageView imageView ;
 
+    TextView date;
+    DatePickerDialog picker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add__event_);
 
-        imageView = (ImageView) findViewById(R.id.imagatest);
+        imageView = (ImageView) findViewById(R.id.imagaView);
         Bitmap imageBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.placeholder);
         imageView.setImageBitmap(imageBitmap);
 
         imageView.getLayoutParams().height = 300;
         imageView.getLayoutParams().width = 300;
 
+        date=(TextView) findViewById(R.id.date);
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(AjouterEventActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                Date d = new Date(year,view.getMonth(),dayOfMonth);
+                                Calendar calendar = Calendar.getInstance();
+                                calendar.set(year, view.getMonth(),dayOfMonth);
+                                d =calendar.getTime();
+                                date.setText(new SimpleDateFormat("d MMMM  HH:MM").format(d));
+
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
     }
 
-    public void onClick(View view) {
-        Intent intent = new Intent();
-// Show only images, no videos or anything else
-        intent.setType("image/*");
-        intent.setAction(Intent.ACTION_GET_CONTENT);
-// Always show the chooser (if there are multiple options available)
-        startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
-    }
-
-    public void takePhoto(View view){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
-    }
 
     public void startDialog(View view) {
         AlertDialog.Builder myAlertDialog = new AlertDialog.Builder(
                 this, R.style.Theme_AppCompat_DayNight_Dialog);
-        myAlertDialog.setTitle("Upload Pictures Option");
-        myAlertDialog.setMessage("How do you want to set your picture?");
+        myAlertDialog.setTitle("Choix de l'image");
+        myAlertDialog.setMessage("Où voulez-vous choisir votre image ?");
 
-
-
-        myAlertDialog.setPositiveButton("Gallery",
+        myAlertDialog.setPositiveButton("Gallerie",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface arg0, int arg1) {
                         Intent intent = new Intent();
-// Show only images, no videos or anything else
                         intent.setType("image/*");
                         intent.setAction(Intent.ACTION_GET_CONTENT);
-// Always show the chooser (if there are multiple options available)
                         startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_IMAGE);
                     }
                 });
 
-        myAlertDialog.setNegativeButton("Camera",
+        myAlertDialog.setNegativeButton("Caméra",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface arg0, int arg1) {
                         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
